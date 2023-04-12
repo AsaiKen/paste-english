ab2d5cf8d8cf579b42285b2cebaa9174457f576e
askn <tc535mr2@gmail.com>
2023-04-13 06:04:13 +0900

    init

---
+++ background.js
@@ -0,0 +1,80 @@
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: 'pasteEnglish',
+    title: '日本語→英語 で貼り付け',
+    contexts: ['selection']
+  });
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === 'pasteEnglish') {
+    chrome.tabs.sendMessage(tab.id, {
+      action: 'pasteEnglish'
+    });
+  }
+});
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === 'translateText') {
+    // DeepL APIを使用してテキストを翻訳する関数
+    translateText(request.text).then(translatedText => {
+      sendResponse({translatedText: translatedText});
+    });
+    // 応答を非同期で処理するために true を返す
+    return true;
+  }
+});
+
+async function translateText(text) {
+  console.log({text});
+  const requestUrl = 'https://api.deepl.com/jsonrpc?method=LMT_handle_jobs';
+  const requestBody = {
+    'jsonrpc': '2.0',
+    'method': 'LMT_handle_jobs',
+    'params': {
+      'jobs': [{
+        'kind': 'default',
+        'sentences': [{'text': text, 'id': 0, 'prefix': ''}],
+        'raw_en_context_before': [],
+        'raw_en_context_after': [],
+        'preferred_num_beams': 4
+      }],
+      'lang': {
+        'preference': {'weight': {}, 'default': 'default'},
+        'source_lang_user_selected': 'JA',
+        'target_lang': 'EN'
+      },
+      'priority': 1,
+      'commonJobParams': {'regionalVariant': 'en-US', 'mode': 'translate', 'browserType': 1},
+      'timestamp': new Date().getTime(),
+    },
+    'id':
+        Math.floor(10000000 + Math.random() * 90000000)
+  };
+
+  try {
+    const response = await fetch(requestUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': '*/*',
+        'Origin': 'https://www.deepl.com',
+        'Referer': 'https://www.deepl.com/',
+        'Accept-Language': 'ja,en-US;q=0.9,en;q=0.8'
+      },
+      body: JSON.stringify(requestBody)
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      const translatedText = data.result?.translations[0]?.beams[0]?.sentences[0]?.text;
+      return translatedText;
+    } else {
+      console.error('Request failed:', response.statusText);
+      return text;
+    }
+  } catch (error) {
+    console.error('Request error:', error);
+    return text;
+  }
+}
---
+++ content.js
@@ -0,0 +1,30 @@
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === 'pasteEnglish') {
+    const selectedText = window.getSelection().toString();
+    const inputElement = document.activeElement;
+    if (selectedText && inputElement) {
+      // テキストの長さが300文字を超える場合、アラートを表示し、処理を終了します。
+      if (selectedText.length > 300) {
+        alert(`一度に翻訳できる日本語は300文字以内です。(${selectedText.length}文字)`);
+        return;
+      }
+
+      chrome.runtime.sendMessage({action: 'translateText', text: selectedText}, response => {
+        const translatedText = response.translatedText;
+        const selectionStart = inputElement.selectionStart;
+        const selectionEnd = inputElement.selectionEnd;
+        const inputValue = inputElement.value;
+        const newText = inputValue.substring(0, selectionEnd) + translatedText + inputValue.substring(selectionEnd);
+
+        inputElement.value = newText;
+        inputElement.selectionStart = inputElement.selectionEnd = selectionEnd + translatedText.length;
+
+        // 応答を返す
+        sendResponse({success: true});
+      });
+
+      // 非同期応答を示します
+      return true;
+    }
+  }
+});
