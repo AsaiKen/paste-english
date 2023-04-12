abb9b91e237deaa1f6c1fc4718d55bf983cc10c8
askn <tc535mr2@gmail.com>
2023-04-13 08:34:26 +0900

    check start and end

--- content.js
+++ content.js
@@ -1,18 +1,29 @@
 function triggerTranslation(sendResponse) {
   const selectedText = window.getSelection().toString();
-  if (selectedText && (selectedText.length === 0 || selectedText.length > 300)) {
+  const inputElement = document.activeElement;
+  const selectionStart = inputElement.selectionStart;
+  const selectionEnd = inputElement.selectionEnd;
+  console.log({
+    selectedText,
+    selectionStart,
+    selectionEnd,
+    tagName: inputElement.tagName,
+    tagType: inputElement.type
+  });
+  if (selectedText.length === 0 || selectionStart === null || selectionEnd === null) {
+    alert('選択したテキストを取得できませんでした。');
+    return;
+  } else if (selectedText.length > 300) {
     alert(`一度に翻訳できる日本語は300文字以内です。(${selectedText.length}文字)`);
     return;
   }
-  const inputElement = document.activeElement;
-  if (selectedText && inputElement) {
+  if (selectedText.length > 0 && inputElement) {
     // テキストの長さが300文字を超える場合、アラートを表示し、処理を終了します。
-    const selectionStart = inputElement.selectionStart;
-    const selectionEnd = inputElement.selectionEnd;
     const inputValue = inputElement.value;
     inputElement.value = inputValue.substring(0, selectionEnd) + '\n(翻訳中...)\n' + inputValue.substring(selectionEnd);
     chrome.runtime.sendMessage({ action: 'translateText', text: selectedText }, response => {
       const translatedText = response.translatedText;
+      console.log({ translatedText });
       if (!translatedText) {
         alert(`翻訳に失敗しました。再度実行してください。`);
         return;
