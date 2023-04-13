61f84ae819f7850a72ac17a1ba184d164fa60ad6
askn <tc535mr2@gmail.com>
2023-04-14 02:22:36 +0900

    div contenteditable="true"対応

--- content.js
+++ content.js
@@ -1,9 +1,22 @@
 function triggerTranslation(sendResponse) {
-  const selectedText = window.getSelection().toString();
   const inputElement = document.activeElement;
-  const selectionStart = inputElement.selectionStart;
-  const selectionEnd = inputElement.selectionEnd;
+  let selectedText = window.getSelection().toString();
+
+  let inputValue;
+  let selectionStart;
+  let selectionEnd;
+  if (inputElement.tagName === 'DIV') {
+    inputValue = inputElement.innerText;
+    const selection = window.getSelection();
+    selectionStart = inputValue.indexOf(selectedText, Math.min(selection.anchorOffset, selection.focusOffset));
+    selectionEnd = selectionStart + selectedText.length;
+  } else {
+    inputValue = inputElement.value;
+    selectionStart = inputElement.selectionStart;
+    selectionEnd = inputElement.selectionEnd;
+  }
   console.log({
+    inputValue,
     selectedText,
     selectionStart,
     selectionEnd,
@@ -15,9 +28,12 @@ function triggerTranslation(sendResponse) {
     return;
   }
   if (selectedText.length > 0 && inputElement) {
-    // テキストの長さが300文字を超える場合、アラートを表示し、処理を終了します。
-    const inputValue = inputElement.value;
-    inputElement.value = inputValue.substring(0, selectionEnd) + '(翻訳中...)' + inputValue.substring(selectionEnd);
+    const tmpText = inputValue.substring(0, selectionEnd) + '(翻訳中...)' + inputValue.substring(selectionEnd);
+    if (inputElement.tagName === 'DIV') {
+      inputElement.textContent = tmpText;
+    } else {
+      inputElement.value = tmpText;
+    }
     chrome.runtime.sendMessage({ action: 'translateText', text: selectedText }, response => {
       const translatedText = response.translatedText;
       // console.log({ translatedText });
@@ -26,10 +42,25 @@ function triggerTranslation(sendResponse) {
         return;
       }
       const newText = inputValue.substring(0, selectionEnd) + translatedText + inputValue.substring(selectionEnd);
-      inputElement.value = newText;
-      // 選択状態を保持する
-      inputElement.selectionStart = selectionStart;
-      inputElement.selectionEnd = selectionEnd;
+      if (inputElement.tagName === 'DIV') {
+        inputElement.textContent = newText;
+        const textNode = inputElement.firstChild;
+        // Rangeオブジェクトを作成
+        const range = document.createRange();
+        // 選択範囲を設定（例: テキストノードの先頭から3文字目まで）
+        range.setStart(textNode, selectionStart);
+        range.setEnd(textNode, selectionEnd);
+        // 現在の選択範囲をクリア
+        const selection = window.getSelection();
+        selection.removeAllRanges();
+        // 新しい選択範囲を追加
+        selection.addRange(range);
+      } else {
+        inputElement.value = newText;
+        // 選択状態を保持する
+        inputElement.selectionStart = selectionStart;
+        inputElement.selectionEnd = selectionEnd;
+      }
       // 応答を返す
       if (sendResponse) {
         sendResponse(true);
